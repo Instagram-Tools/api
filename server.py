267f7b654c77937f0aefc7bf4c97ecba5c28f386
Gott50 <xxGott50xx@gmail.com>
2018-05-30 14:27:19 +0200

    setup Server Files

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import request
+from flask_sqlalchemy import SQLAlchemy
+from config import BaseConfig
+import json
+
+app = Flask(__name__)
+app.config.from_object(BaseConfig)
+db = SQLAlchemy(app)
+
+import models
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    try:
+        data = json.loads(request.data)
+
+        return data
+    except Exception as exc:
+        return str(exc)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
