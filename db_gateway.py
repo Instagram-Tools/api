f3be9d0b902f43c3be2e17aad3faad6b02b03db8
Gott50 <xxGott50xx@gmail.com>
2018-06-03 12:24:56 +0200

    refactor: move delete_time_tables() to update

--- db_gateway.py
+++ db_gateway.py
@@ -28,25 +28,18 @@ class DB_GateWay:
     def update_user(self, data):
         user = self.models.User(username=data.get("username"), password=data.get("password"),
                                 settings=data.get("settings"), timestamp=timestamp())
-        first: self.models.User = self.models.User.query.filter_by(username=user.username).first()
-        if first:
-            first.password = user.password
-            first.settings = user.settings
-            first.timestamp = user.timestamp
-
-            self.models.TimeTable.query.filter_by(user_id=first.id).delete()
-
-            self.db.session.commit()
-            return first
-
         self.db.session.add(user)
         self.db.session.commit()
         return user
 
     def update_timetable(self, user, data):
         timetable = data.get("timetable", [])
+        self.deleteTimeTables(user)
         for i in range(0, len(timetable), 2):
             timetable = self.models.TimeTable(user_id=user.id, start=parse_datetime(timetable[i]),
                                               end=parse_datetime(timetable[i + 1]))
             self.db.session.add(timetable)
         self.db.session.commit()
+
+    def delete_time_tables(self, user):
+        self.models.TimeTable.query.filter_by(user_id=user.id).delete()
