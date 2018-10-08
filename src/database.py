43dbd8196aa3fd92ee76ea26fa4c1e23b8f666f9
Gott50 <xxGott50xx@gmail.com>
2018-10-08 07:26:54 +0200

    add credit to User in DB Schema for Referrals

--- src/database.py
+++ src/database.py
@@ -52,6 +52,7 @@ class DB:
                 paid = db.Column(db.Boolean, default=False)
                 started = db.Column(db.Boolean, default=True)
                 user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+                credit = db.Column(db.Integer, default=0)
 
                 def __repr__(self):
                     return '<Account %r>' % self.username
