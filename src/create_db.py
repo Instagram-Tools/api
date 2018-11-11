4edadd74e44b89f597b89ec7865272b21b2238fb
Gott50 <xxGott50xx@gmail.com>
2018-11-11 14:24:50 +0100

    remove init_discount_codes()

--- src/create_db.py
+++ src/create_db.py
@@ -7,14 +7,3 @@ from server import db, database
 # Create the database and the database table
 print("db.create_all()")
 db.create_all()
-
-def init_discount_codes():
-    try:
-        code = database.models.DiscountCode(code="CASHBACK30", discount=20, active=True)
-        db.session.add(code)
-        db.session.commit()
-    except Exception as exc:
-        print("Exception while init_discount_codes():")
-        print(exc)
-
-init_discount_codes()
