83e11c2eea7b2b80f1a0918a3b1d1003fd15bb96
Gott50 <xxGott50xx@gmail.com>
2019-03-25 12:34:29 +0100

    update DB Schema

--- src/database.py
+++ src/database.py
@@ -52,7 +52,7 @@ class DB:
                 __tablename__ = 'account'
 
                 id = db.Column(db.Integer, primary_key=True)
-                username = db.Column(db.String(80), unique=True, nullable=False)
+                username = db.Column(db.String(80), unique=True)
                 password = db.Column(db.String(80), nullable=False)
                 settings = db.Column(db.Text(), nullable=False)
                 timetables = db.relationship('TimeTable', backref='account', lazy=True)
@@ -62,7 +62,7 @@ class DB:
                 user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
                 credit = db.Column(db.Integer, default=0)
 
-                subscription = db.Column(db.String(80), nullable=False, unique=True)
+                subscription = db.Column(db.String(80), unique=True)
                 paid = db.Column(db.Boolean, default=False)
 
                 def __repr__(self):
