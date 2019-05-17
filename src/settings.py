f2caf9380f16ee17308a6d9f61843680a28c8eea
Gott50 <xxGott50xx@gmail.com>
2019-05-17 14:59:05 +0200

    fix static_url_path='/api/static'

--- src/settings.py
+++ src/settings.py
@@ -3,7 +3,7 @@ from config import BaseConfig
 from db_gateway import DB_GateWay
 import database as database_db
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/api/static')
 app.config.from_object(BaseConfig)
 database = database_db.DB(app)
 db = database.db
